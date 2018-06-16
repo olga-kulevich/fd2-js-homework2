a24288fe3a62e6fe3f5d2b87a070588ed92a8a74
olga <ol4ik_bel89@mail.ru>
2018-06-16 19:42:30 +0300

    jasmine tests

--- home-work-4-yakusik-maksim/main.spec.js
+++ home-work-4-yakusik-maksim/main.spec.js
@@ -24,17 +24,50 @@ function getPathByHash(hash) {
 
 describe('Test for addition', function () {
 
-    // write tests here
+    it('must return 5 for arguments 2 and 3', function() {
+        expect(addition(2,3)).toBe(5);
+    });
+
+    it('must return "helloWorld" for arguments "hello" and "World"', function() {
+        expect(addition('hello','World')).toBe('helloWorld');
+    });
+
 });
 
 describe('Test for isNull', function () {
 
-    // write tests here
+    it('must return true for null', function() {
+        expect(isNull(null)).toBeTruthy();
+    });
+
+    it('must return false for 0', function() {
+        expect(isNull(0)).toBeFalsy();
+    });
+
+    it('must return false for ""', function() {
+        expect(isNull('')).toBeFalsy();
+    });
+
 });
 
 describe('Test for getGreeting', function () {
 
-    // write tests here
+    it('must contain string "Hello "', function () {
+        var result;
+
+        result = getGreeting('guys');
+
+        expect(result).toContain("Hello ");
+    });
+
+    it('must check and return string', function () {
+        var result;
+
+        result = getGreeting({});
+
+        expect(typeof result === "string").toBe(true);
+    });
+
 });
 
 describe('Test for parseBoolean', function () {
@@ -43,16 +76,34 @@ describe('Test for parseBoolean', function () {
         expect(parseBoolean(' TruE')).toBe(true);
     });
 
-    // write tests here
+    it('must return true for "\tTruE\t" string', function () {
+        expect(parseBoolean('\tTruE\t')).toBe(true);
+    });
+
+    it('must return false for "khghgkTruE" string', function () {
+        expect(parseBoolean('khghgkTruE')).toBe(false);
+    });
+
 });
 
 describe('Test for getPathByHash', function () {
 
-    it('must return ["user", "create"] for "user/create" hash string', function () {
+    it('must return ["user","create"] for "user/create" hash string', function () {
         expect(getPathByHash('user/create')).toEqual(['user', 'create']);
     });
 
-    // write tests here
+    it('must return ["user","create"] for "user/   /create" hash string', function () {
+        expect(getPathByHash('user/   /create')).toEqual(['user','create']);
+    });
+
+    it('must return ["user","abc","create"] for "user/ abc  /create" hash string', function () {
+        expect(getPathByHash('user/ abc  /create')).toEqual(['user','abc','create']);
+    });
+
+    it('must return ["user","abc","create"] for "user/ abc\t /create" hash string', function () {
+        expect(getPathByHash('user/ abc\t /create')).toEqual(['user','abc','create']);
+    });
+
 });
 
 
